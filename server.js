aeac67d028606287663fb9b4fd958e660e1b6495
Nicolas <>
2017-07-16 22:41:33 +0200

    Add Projects page

--- server.js
+++ server.js
@@ -45,6 +45,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+        descriptionProjects: 'Here are all the projects I\'m currently working on'
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: "Unable to handle this request."
